1adb7130f83262cdcbcba92feba27139d94f7ad5
edman-cota <edmancota2017@gmail.com>
2022-10-29 01:35:52 -0600

    fix: resolved unable to find file deleted

--- src/components/Item/ItemMenu.tsx
+++ src/components/Item/ItemMenu.tsx
@@ -1,14 +1,10 @@
-import React from "react"
 import { Flex } from "@chakra-ui/react"
 import RenderDateText from "../RenderDate/RenderDateText"
-import TagIcon from "../Icons/TagIcon"
 
 const ItemMenu = ({ task }: { task: any }): JSX.Element => {
   return (
     <Flex visibility="hidden" flex={1} _groupHover={{ visibility: "visible" }}>
-      <Flex flex={1} justifyContent="center">
-        <TagIcon />
-      </Flex>
+      <Flex flex={1} justifyContent="center"></Flex>
       <Flex flex={1} alignItems="center" justifyContent="center">
         {task?.due !== undefined ? <RenderDateText due={task.due} /> : null}
       </Flex>
