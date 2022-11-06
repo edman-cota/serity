5d4c7884b875aee51b7e84423c812d085e2516c1
edman-cota <edmancota2017@gmail.com>
2022-11-06 16:39:34 -0600

    fix: comment react-moment code that cause the app to crash

--- src/components/Timeline/Time.tsx
+++ src/components/Timeline/Time.tsx
@@ -14,9 +14,9 @@ const Time = ({ createdAt }: Props) => {
 
   return (
     <Text as='span' fontSize='14px' color='rgb(133, 134, 153)'>
-      <Moment fromNow>1976-04-19T12:59-0500</Moment>
-      {/* {new Date(date.getTime()).toLocaleDateString('en-US')} -{' '}
-      {formatAMPM(new Date(date.getTime()))} */}
+      {/* <Moment fromNow>1976-04-19T12:59-0500</Moment> */}
+      {new Date(date.getTime()).toLocaleDateString('en-US')} -{' '}
+      {formatAMPM(new Date(date.getTime()))}
     </Text>
   )
 }
