c764a77b318394a0a68a5f64b14b3a95b5e1b25d
edman-cota <edmancota2017@gmail.com>
2022-11-10 00:50:51 -0600

    fix(navbar): In small screen use vertical dots icon

--- src/components/Menus/ProjectOptionsMenu.tsx
+++ src/components/Menus/ProjectOptionsMenu.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
 // eslint-disable-next-line object-curly-newline
 import { Menu, MenuList, MenuItem, MenuButton, Button } from '@chakra-ui/react'
-import { RiMoreLine } from 'react-icons/ri'
+import { RiMoreLine, RiMore2Line } from 'react-icons/ri'
 import { AiOutlineExport, AiOutlineShareAlt } from 'react-icons/ai'
 import { FormattedMessage } from 'react-intl'
 import DeleteProjectItemModal from '../Modals/DeleteProjectModal'
 import MembersMenuItem from '../Modals/InviteMembersModal'
 import MenuItemEdit from '../Modals/EditProject'
+import { useWindowSize } from 'react-use'
 
 interface Props {
   name: string
@@ -15,24 +14,28 @@ interface Props {
   emoji: string
 }
 
-const ProjectMore = ({ name, id, emoji }: Props) => (
-  <Menu autoSelect={false} placement='bottom'>
-    <MenuButton as={Button}>
-      <RiMoreLine size={20} />
-    </MenuButton>
-    <MenuList>
-      <MenuItemEdit name={name} id={id} emoji={emoji} />
-      <MembersMenuItem projectName={name} />
-      <MenuItem icon={<AiOutlineShareAlt />}>
-        <FormattedMessage id='share' />
-      </MenuItem>
-      <MenuItem icon={<AiOutlineExport />}>
-        <FormattedMessage id='export' />
-      </MenuItem>
+const ProjectMore = ({ name, id, emoji }: Props) => {
+  const { width } = useWindowSize()
 
-      <DeleteProjectItemModal name={name} id={id} />
-    </MenuList>
-  </Menu>
-)
+  return (
+    <Menu autoSelect={false} placement='bottom'>
+      <MenuButton as={Button}>
+        {width >= 768 ? <RiMoreLine size={20} /> : <RiMore2Line size={20} />}
+      </MenuButton>
+      <MenuList>
+        <MenuItemEdit name={name} id={id} emoji={emoji} />
+        <MembersMenuItem projectName={name} />
+        <MenuItem icon={<AiOutlineShareAlt />}>
+          <FormattedMessage id='share' />
+        </MenuItem>
+        <MenuItem icon={<AiOutlineExport />}>
+          <FormattedMessage id='export' />
+        </MenuItem>
+
+        <DeleteProjectItemModal name={name} id={id} />
+      </MenuList>
+    </Menu>
+  )
+}
 
 export default ProjectMore
