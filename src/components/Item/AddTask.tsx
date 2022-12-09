040ee6a54b58d6fa3d199efbd1e1f5dcf7228f63
edman-cota <edmancota2017@gmail.com>
2022-12-08 20:06:14 -0600

    fix: improve add task input box style and formatting

--- src/components/Item/AddTask.tsx
+++ src/components/Item/AddTask.tsx
@@ -4,7 +4,7 @@ import ReactFocusLock from 'react-focus-lock'
 import { BiSquareRounded } from 'react-icons/bi'
 import { useDispatch, useSelector } from 'react-redux'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { useColorModeValue, Textarea, Collapse, Flex } from '@chakra-ui/react'
+import { useColorModeValue, Textarea, Collapse, Flex, Button } from '@chakra-ui/react'
 
 import { RootState } from 'src/store'
 import { auth } from '../../firebase'
@@ -17,9 +17,11 @@ const AddTask = () => {
   const { project } = useGetProject()
   const [title, setTitle] = useState('')
   const dispatch = useDispatch()
+  const cardStyle = useSelector((state: RootState) => state.cardStyle.value)
   const showAddTask = useSelector((state: RootState) => state.showAddTask.value)
   const [height, setHeight] = useState(40)
   const areaRef = useRef(null)
+  const borderRadius = cardStyle === 'checkbox' ? '3px' : '50px'
 
   const handleKeyDown = (event: any) => {
     const keyCode = event.which || event.keyCode
@@ -53,12 +55,23 @@ const AddTask = () => {
         display: 'flex',
         borderRadius: '0.375rem',
         margin: '10px auto',
-        padding: '0 10px',
+        padding: '0 6px',
         background: background,
       }}
     >
-      <Flex width='38px' height='40px' justifyContent='center' alignItems='center'>
-        <BiSquareRounded color='#a0aec0' fontSize={15} />
+      <Flex width='38px' height='44px' justifyContent='center' alignItems='center'>
+        <Button
+          display='flex'
+          justifyContent='center'
+          minW='20px'
+          _hover={{ bg: 'transparent' }}
+          _active={{ bg: 'transparent' }}
+        >
+          <div className='control'>
+            <input readOnly type='checkbox' id='input' className='hidden-xs-up' />
+            <label htmlFor='input' style={{ borderRadius: borderRadius }} className='cbx' />
+          </div>
+        </Button>
       </Flex>
       <ReactFocusLock>
         <Textarea
@@ -68,7 +81,9 @@ const AddTask = () => {
           name='w3review'
           rows={1}
           cols={200}
+          marginTop='3px'
           w='full'
+          fontSize='15px'
           border='none'
           h={`${height}px`}
           ref={areaRef}
